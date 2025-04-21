6e4a1b0e21919bb045d7177c6375c9487aedcf49
shef-04 <yuta.ochiai.0410@gmail.com>
2025-04-22 00:21:26 +0900

    Update test1.py

--- project_01/test1.py
+++ project_01/test1.py
@@ -1,4 +1,4 @@
 import torch
 
-torch.cuda.is_available()
-
+print("CUDA available:", torch.cuda.is_available())
+print("CUDA device name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "CPU only")
